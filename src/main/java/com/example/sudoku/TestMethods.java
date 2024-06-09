3d1161a3108fda88dd48a140ff5a795f21cb36f7
agorski1 <artur.gorski5@wp.pl>
2024-06-09 16:00:00 +0200

    Added main class and test methods

---
+++ src/main/java/com/example/sudoku/TestMethods.java
@@ -0,0 +1,12 @@
+package com.example.sudoku;
+
+public class TestMethods {
+    public static void displayArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
